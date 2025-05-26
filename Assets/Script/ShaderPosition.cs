021c8375ed27c2e8b83762636b8dd1084272b4f3
GUS <gustavolimamendest@outlook.com>
2025-05-26 19:06:42 -0300

    Animation Fire and Att Shader Elipse Feature

--- Assets/Script/ShaderPosition.cs
+++ Assets/Script/ShaderPosition.cs
@@ -5,19 +5,19 @@ using UnityEngine;
 [ExecuteInEditMode]
 public class ShaderPosition : MonoBehaviour
 {
-    public float radius = 1f;
+    public Vector2 radius = new Vector2(1f, 1f);
     public ParticleSystem particleSystem;
 
     void Update()
     {
         Shader.SetGlobalVector("_Position", transform.position);
-        Shader.SetGlobalFloat("_Radius", radius);
+        Shader.SetGlobalVector("_Radius", new Vector4(radius.x, radius.y, 0f, 0f));
 
         // Atualiza o raio do shape do Particle System
         if (particleSystem != null)
         {
             var shape = particleSystem.shape;
-            shape.radius = radius - 0.5f;
+            shape.scale = new Vector3(radius.x, radius.y, 1f);
         }
 
     }
